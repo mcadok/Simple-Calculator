bb72f2136bf4bf2b98b1c495744f8242f700dbe5
Adrian_Augustyn <adrian.jan.augustyn@gmail.com>
2025-06-08 17:07:11 +0200

    Update simple_calc_script.js

--- simple_calc_script.js
+++ simple_calc_script.js
@@ -1,90 +1,51 @@
-function dodaj_do_input(wartosc_z_pola){
-    let zapis_input = document.getElementById('wpis');
-    zapis_input.value += wartosc_z_pola;
+function dod_input(zaw_guzika){
+    document.getElementById("mod1").value += zaw_guzika;
 }
 
-let Wartosc1 = 0;
+let aktualna_op = '';
 let wynik = 0;
-let pierwsze_czy_kolejne = 0;
+let o_ile_zmiana = 0;
+let powtorzenia = 0;
 
-let aktualna_operacja = '';
+function wybor_op(op_X){
+        console.log(wynik);
+        wynik = document.getElementById("mod1").value;
+        document.getElementById("mod1").value = '';
+        aktualna_op = op_X;
+        powtorzenia = 0;
+    }
 
-function wyborOpX(operacja){
-    let zapis_wartosci = document.getElementById('wpis');
-    Wartosc1 = zapis_wartosci.value;
-    zapis_wartosci.value = '';
-    aktualna_operacja = operacja;
-    pierwsze_czy_kolejne = 0;
-}
-
-function RownaSie(){
-    if(aktualna_operacja == 'dodawanie')
-    {
-        if(pierwsze_czy_kolejne == 0){
-            let zapis_wartosc2 = document.getElementById('wpis');
-            Wartosc2 = zapis_wartosc2.value;
-            wynik = Number(Wartosc1) + Number(Wartosc2);
-            zapis_wartosc2.value = wynik;
-            console.log(wynik);
-            pierwsze_czy_kolejne++;}
-        else{
-            wynik = Number(wynik) + Number(Wartosc2);
-            document.getElementById('wpis').value = wynik;
-            console.log(aktualna_operacja);
-            }
+function rowna_sie(){
+    if(aktualna_op=='dod'){
+        if(powtorzenia == 0){o_ile_zmiana = document.getElementById("mod1").value;}
+        wynik = Number(wynik) + Number(o_ile_zmiana);
+        document.getElementById("mod1").value = wynik;
+        powtorzenia++;
     }
-    if(aktualna_operacja == 'odejmowanie')
-    {
-        if(pierwsze_czy_kolejne == 0){
-            let zapis_wartosc2 = document.getElementById('wpis');
-            Wartosc2 = zapis_wartosc2.value;
-            wynik = Number(Wartosc1) - Number(Wartosc2);
-            zapis_wartosc2.value = wynik;
-            console.log(wynik);
-            pierwsze_czy_kolejne++;}
-        else{
-            wynik = Number(wynik) - Number(Wartosc2);
-            document.getElementById('wpis').value = wynik;
-            console.log(aktualna_operacja);
-            }
+    if(aktualna_op=='ode'){
+        if(powtorzenia == 0){o_ile_zmiana = document.getElementById("mod1").value;}
+        wynik = Number(wynik) - Number(o_ile_zmiana);
+        document.getElementById("mod1").value = wynik;
+        powtorzenia++;
     }
-    if(aktualna_operacja == 'dzielenie')
-    {
-        if(pierwsze_czy_kolejne == 0){
-            let zapis_wartosc2 = document.getElementById('wpis');
-            Wartosc2 = zapis_wartosc2.value;
-            wynik = Number(Wartosc1) / Number(Wartosc2);
-            zapis_wartosc2.value = wynik;
-            console.log(wynik);
-            pierwsze_czy_kolejne++;}
-        else{
-            wynik = Number(wynik) / Number(Wartosc2);
-            document.getElementById('wpis').value = wynik;
-            console.log(aktualna_operacja);
-            }
+    if(aktualna_op=='mno'){
+        if(powtorzenia == 0){o_ile_zmiana = document.getElementById("mod1").value;}
+        wynik = Number(wynik) * Number(o_ile_zmiana);
+        document.getElementById("mod1").value = wynik;
+        powtorzenia++;
     }
-    if(aktualna_operacja == 'mnozenie')
-    {
-        if(pierwsze_czy_kolejne == 0){
-            let zapis_wartosc2 = document.getElementById('wpis');
-            Wartosc2 = zapis_wartosc2.value;
-            wynik = Number(Wartosc1) * Number(Wartosc2);
-            zapis_wartosc2.value = wynik;
-            console.log(wynik);
-            pierwsze_czy_kolejne++;}
-        else{
-            wynik = Number(wynik) * Number(Wartosc2);
-            document.getElementById('wpis').value = wynik;
-            console.log(aktualna_operacja);
-            }
+    if(aktualna_op=='dzi'){
+        if(powtorzenia == 0){o_ile_zmiana = document.getElementById("mod1").value;}
+        wynik = Number(wynik) / Number(o_ile_zmiana);
+        document.getElementById("mod1").value = wynik;
+        powtorzenia++;
     }
 }
 
-function ResetC(){
-    aktualna_operacja = ''
+function reset_C(){
+    aktualna_op = '';
     wynik = 0;
-    Wartosc1 = 0;
-    Wartosc2 = 0;
-    pierwsze_czy_kolejne = 0;
-    document.getElementById('wpis').value = '';
+    o_ile_zmiana = 0;
+    powtorzenia = 0;
+    document.getElementById("mod1").value = '';
 }
